2594e37a3d6d0472d54749a1511df6c791405817
Jonatas Oliveira de Araujo <jonatasoliveiraaraujo@id.uff>
2024-04-07 23:39:11 -0300

    testes

--- src/time_travel_wave2.py
+++ src/time_travel_wave2.py
@@ -1,20 +1,25 @@
 import numpy as np
 
 # Equação para onda direta
-def direct_wave(velocity, dtf, dt0, offset_max, space, nx):
+def direct_wave(velocity, rec, src, dtf, dt0, offset_max, space):
     
     """ velocity = velocidade de propagação da onda
         dtf = tempo final de chegada da onda
         dt0 = tempo inicial de propagação da onda
         offset_max = maior distancia entre a fonte e receptor
         space = espaçamento entre os receptores """
+        
+    distance = np.zeros(len(rec))
 
-    start_dist = offset_max + space
+    for i in range(len(rec)):
+        distance[i] = src[0] - rec[i]
+
+    start_distance = offset_max + space
     end_dist = (velocity * (dtf - dt0)) + offset_max
-    x = np.linspace(start_dist, end_dist, nx)
-    deslocamento = offset_max - velocity * dt0
-    t = (x - deslocamento)/velocity
-    return x, t
+    x = np.linspace(start_distance, end_dist, len(rec))
+
+    t = ((distance - start_distance)/velocity) - dt0
+    return x, t, distance
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
@@ -36,8 +41,17 @@ def refracted_waves(velocity1, velocity2, offset_max, space, dtf, dt0, nx):
 # ------------------------------------------------------------------------
 
 # Equação para onda refletida tipo1
-def reflected_wave(velocity, t0, x ):
-   
-    x0 = 584
-    t = np.sqrt((((x - x0)**2) / velocity**2) + t0**2)
-    return t
+def reflected_wave(velocity,t0, src, rec):
+    #start_dist = offset_max + space
+    #end_dist = (velocity * (distance - dt0)) + offset_max  # Conferir no sismograma qual velocidade usar
+    
+    distance = np.zeros(len(rec))
+
+    for i in range(len(rec)):
+        distance[i] = src[0] - rec[i]
+    
+    #x = np.linspace(offset_max, 1160, nx)
+    #deslocamento = offset_max - velocity * dt0  # Conferir no sismograma qual velocidade usar
+
+    t = np.sqrt((((distance - 584)**2) / velocity**2) + t0**2)
+    return t, distance
--- src/time_wave_example.py
+++ src/time_wave_example.py
@@ -23,9 +23,10 @@ x3, t3 = direct_wave(vs[1], dtf, dt0, offset_max, space, nx)
 x_refr, t_refr = refracted_waves(vp[1], vp[2], offset_max, space, dtf, dt0, nx)
 
 # Onda refletida
-dt0_refl = 0.4
-x_refl1, t_refl1 = reflected_wave(vp[2], offset_max, space, dtf, dt0_refl, nx )
-x_refl2, t_refl2 = reflected_wave(vp[3], offset_max, space, dtf, dt0_refl, nx )
+t0_1 = 0.462
+x_refl1 = np.linspace(584, 1030, 145)
+t_refl1 = reflected_wave(vp[1], t0_1, x_refl1 )
+#x_refl2, t_refl2 = reflected_wave(vp[3], offset_max, space, dtf, dt0_refl, nx )
 
 plt.figure(figsize=(10,10))
 # plot onda direta
@@ -33,9 +34,9 @@ plt.plot(x1, t1, 'blue')
 plt.plot(x2, t2, 'red')
 plt.plot(x3, t3, 'green')
 
-plt.plot(-x1 + 1160, t1, 'blue')
-plt.plot(-x2 + 1160, t2, 'red')
-plt.plot(-x3 + 1160, t3, 'green')
+# plt.plot(-x1 + 1160, t1, 'blue')
+# plt.plot(-x2 + 1160, t2, 'red')
+# plt.plot(-x3 + 1160, t3, 'green')
 
 # # plot onda refratada
 # plt.plot(x_refr, t_refr, 'black')  # Está sobre t2
@@ -43,10 +44,10 @@ plt.plot(-x3 + 1160, t3, 'green')
 
 # plot onda relfetida
 plt.plot(x_refl1,t_refl1, 'yellow')
-plt.plot(x_refl2,t_refl2, "orange")
+# plt.plot(x_refl2,t_refl2, "orange")
 
-plt.plot(-x_refl1+1160,t_refl1, 'yellow' )
-plt.plot(-x_refl2 + 1160,t_refl2, 'orange')
+# plt.plot(-x_refl1+1160,t_refl1, 'yellow' )
+# plt.plot(-x_refl2 + 1160,t_refl2, 'orange')
 
 plt.grid()
 plt.ylim(0.8, 0)
