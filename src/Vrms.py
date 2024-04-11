875fd3c4de870534f9a9ceebcff44d55b8242f12
Jonatas2002 <jonatasoliveiraaraujo@id.uff.br>
2024-04-11 17:45:04 -0300

    ...

--- src/Vrms.py
+++ src/Vrms.py
@@ -1,14 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def analytical_reflections(v, z, x):
+def analytical_reflections(v, z):
     Tint = 2.0 * z / v[:-1]
     Vrms = np.zeros(len(z))
-    reflections = np.zeros((len(z), len(x)))
     for i in range(len(z)):
         Vrms[i] = np.sqrt(np.sum(v[:i+1]**2 * Tint[:i+1]) / np.sum(Tint[:i+1]))
-        reflections[i] = np.sqrt(x**2.0 + 4.0*np.sum(z[:i+1])**2) / Vrms[i]
-    return reflections, Vrms
+    return Vrms
 
 n_receivers = 145
 total_time = 0.8
@@ -23,13 +21,6 @@ nx = int(n_receivers / 2) + 1
 z = np.array([200, 400])
 v = np.array([800, 1200, 2200])
 
-x = np.linspace(0, nx*dx, nx)
-
-reflections, Vrms = analytical_reflections(v, z, x)
-
-plt.figure()
-plt.plot(reflections)
-
-plt.show()
+Vrms = analytical_reflections(v, z)
 
 print((Vrms))
